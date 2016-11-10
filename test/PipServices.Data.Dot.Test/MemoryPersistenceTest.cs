d703293baab931f683b586188558faa2b7488c42
vovant <vovantigo@gmail.com>
2016-11-10 16:04:20 +0200

    Tests were added to classic .Net version

---
+++ test/PipServices.Data.Dot.Test/MemoryPersistenceTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using PipServices.Data.Memory;
+using PipServices.Commons.Config;
+using Xunit;
+
+namespace PipServices.Data.Test
+{
+    public sealed class MemoryPersistenceTest
+    {
+        private static MemoryPersistence<PersistenceFixture.Dummy, string> Db { get; } = new MemoryPersistence<PersistenceFixture.Dummy, string>();
+        private static PersistenceFixture Fixture { get; set; }
+
+        private PersistenceFixture GetFixture()
+        {
+            return new PersistenceFixture(Db, Db, Db, Db, Db, Db, Db, Db);
+        }
+
+        public MemoryPersistenceTest()
+        {
+            if (Db == null)
+                return;
+
+            Db.Configure(new ConfigParams());
+
+            var task = Db.OpenAsync(null, CancellationToken.None);
+            task.Wait();
+
+            task = Db.ClearAsync(null, CancellationToken.None);
+            task.Wait();
+
+            Fixture = GetFixture();
+        }
+
+        [Fact]
+        public void TestCrudOperations()
+        {
+            if (Fixture == null) return;
+
+            var task = Fixture.TestCrudOperationsAsync(CancellationToken.None);
+            task.Wait();
+        }
+
+        [Fact]
+        public void TestMultithreading()
+        {
+            if (Fixture == null) return;
+
+            var task = Fixture.TestMultithreading(CancellationToken.None);
+            task.Wait();
+        }
+    }
+}
---
+++ test/PipServices.Data.Dot.Test/MongoDbPersistenceTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using PipServices.Data.MongoDb;
+using PipServices.Commons.Config;
+using PipServices.Commons.Refer;
+using Xunit;
+
+namespace PipServices.Data.Test
+{
+    public sealed class MongoDbPersistenceTest
+    {
+        private static MongoDbPersistence<PersistenceFixture.Dummy, string> Db { get; } = new MongoDbPersistence<PersistenceFixture.Dummy, string>("dummies", new Descriptor("pip-services-data", "prsistance", "mongodb", "1.0"));
+        private static PersistenceFixture Fixture { get; set; }
+
+        private PersistenceFixture GetFixture()
+        {
+            return new PersistenceFixture(Db, Db, Db, Db, Db, Db, Db, Db);
+        }
+
+        public MongoDbPersistenceTest()
+        {
+            if (Db == null)
+                return;
+
+            Db.Configure(ConfigParams.FromTuples(
+                "connection.type", "mongodb",
+                "connection.database", "test",
+                "connection.uri", ""));
+
+            var task = Db.OpenAsync(null, CancellationToken.None);
+            task.Wait();
+
+            task = Db.ClearAsync(null, CancellationToken.None);
+            task.Wait();
+
+            Fixture = GetFixture();
+        }
+
+        [Fact]
+        public void TestCrudOperations()
+        {
+            if (Fixture == null) return;
+
+            var task = Fixture.TestCrudOperationsAsync(CancellationToken.None);
+            task.Wait();
+        }
+
+        [Fact]
+        public void TestMultithreading()
+        {
+            if (Fixture == null) return;
+
+            var task = Fixture.TestMultithreading(CancellationToken.None);
+            task.Wait();
+        }
+    }
+}
